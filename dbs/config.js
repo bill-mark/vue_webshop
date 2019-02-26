9a6848bdd4d0d4926abc1a9e7374c5be6b7636c4
bill.mark <2286449127@qq.com>
2019-02-26 17:59:15 +0800

    mongoose第一节

--- dbs/config.js
+++ dbs/config.js
@@ -1,3 +1,5 @@
-export default ({
-	dbs: 'mongodb://127.0.0.1:27017/dbs'
-})
+module.exports = {
+	dbs: 'mongodb://127.0.0.1:27017/dbs',
+
+}
+//1. 新建数据量,数据库名称dbs
---
+++ dbs/models/person.js
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose'
+
+let personSchema = new mongoose.Schema({
+	name:String,
+	age:Number
+})
+module.exports = mongoose.model('Person',personSchema)
